fa6f60d77c3730de4f32720e406ccaa725cd402b
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2021-11-11 20:46:25 -0600

    [add] home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./img/logo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contenedor-app">
+        <img src={logo} width="100" height="50" alt="Eisai Connect" />
+        <h1>En construcción...</h1>
+      </div>
     </div>
   );
 }
